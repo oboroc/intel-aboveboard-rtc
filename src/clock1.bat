daef985cef237dbea6209c7397e2725addfc86bf
Adrian Oboroc <oboroc@users.noreply.github.com>
2024-05-28 18:08:21 -0400

    Update clock1.bat

--- src/clock1.bat
+++ src/clock1.bat
@@ -1,10 +1,9 @@
 if exist masm.err del masm.err
 if exist link.err del link.err
-if exist clock.asm msdos.exe d:\dosbox\masm51\masm /DTWEAK /DTWEAK2 /V /Z /ZI clock,,,; 2> masm.err
-rem if exist clock.asm masm /V /Z /ZI clock,,,; 2> masm.err
-if exist clock.crf msdos.exe d:\dosbox\masm51\cref clock;
-if exist clock.obj msdos.exe d:\dosbox\masm51\link clock,,,; 2> link.err
-if exist clock.exe msdos.exe D:\dosbox\WATCOM\BINW\EXE2BIN.exe clock.exe clock.sys
+if exist clock.asm msdos.exe -v5.0 d:\dosbox\masm51\masm /DTWEAK /DTWEAK2 /V /Z /ZI clock,,,; 2> masm.err
+if exist clock.crf msdos.exe -v5.0 d:\dosbox\masm51\cref clock;
+if exist clock.obj msdos.exe -v5.0 d:\dosbox\masm51\link clock,,,; 2> link.err
+if exist clock.exe msdos.exe -v5.0 d:\dosbox\masm51\EXE2BIN.exe clock.exe clock.sys
 if exist clock.obj del clock.obj
 if exist clock.exe del clock.exe
 if exist clock.crf del clock.crf
